ca8d6fec6be889add7d7a6336fa311a23d773944
lifeixiong <lifeixiong@outlook.com>
2018-09-13 21:04:13 +0800

    add hello-world

---
+++ hello-world/app.js
@@ -0,0 +1,15 @@
+const Koa = require("koa")
+
+const app = new Koa();
+
+app.use(async function (ctx) {
+    ctx.body = "helloword!";
+})
+
+if (!module.parent) {
+    app.listen(3000, ()=> {
+        console.log(">>> server listen on http://localhost:3000");
+    });
+}
+
+module.exports = app;
